3680babbd50ff5d0a3e05a982bb96a1f6b0eaa88
Vandana <Vandanapawarsinghal@gmail.com>
2021-01-02 13:46:04 +0530

    color changes

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("blue");
+      fill("yellow");
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
